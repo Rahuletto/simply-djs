7368e38030a09309f15bea7e8d4aaa75e135b8dc
Maf <dragoncatm@gmail.com>
2021-10-26 12:54:26 +0200

    Improving chatbot
    Following improvements:
     - Better Error handling when API is offline
     - More information is given to the API for better responses (like birthday)
     - Using URL class with URLSearchParams for support for unescaped Usernames/Botnames/Messages
     - Removed unnecessary RegExp's

--- src/chatbot.js
+++ src/chatbot.js
@@ -2,18 +2,18 @@ const Discord = require('discord.js')
 const fetch = require('node-fetch')
 
 /**
- * @param {Discord.Client} client
- * @param {Discord.Message} message
- * @param {import('../index').chatbotOptions} options
- */
-
-/**
  --- options ---
  
 toggle => Boolean
 chid => (Channel ID) String
 name => String
 developer => String
+ */
+
+/**
+ * @param {Discord.Client} client
+ * @param {Discord.Message} message
+ * @param {import('../index').chatbotOptions} options
  */
 
 async function chatbot(client, message, options = {}) {
@@ -39,33 +39,39 @@ async function chatbot(client, message, options = {}) {
 		//Return if the channel of the message is not a chatbot channel
 		if (!channels.includes(message.channel.id)) return
 
-		const botName = options.name ?? client.user.username,
-			developer = options.developer ?? 'Rahuletto#0243',
-			ranges = [
-				'\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
-				'\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
-				'\ud83d[\ude80-\udeff]' // U+1F680 to U+1F6FF
-			]
+		const ranges = [
+			'\ud83c[\udf00-\udfff]', // U+1F300 to U+1F3FF
+			'\ud83d[\udc00-\ude4f]', // U+1F400 to U+1F64F
+			'\ud83d[\ude80-\udeff]' // U+1F680 to U+1F6FF
+		]
 
-		let input = message.content.replace(new RegExp(ranges.join('|'), 'g'), '.')
+		let input = message.cleanContent.replace(
+			new RegExp(ranges.join('|'), 'g'),
+			'.'
+		)
 
 		//Replacing Emojis
-		const hasEmoteRegex = /<a?:.+:\d+>/gm
-		const emoteRegex = /<:.+:(\d+)>/gm
-		const animatedEmoteRegex = /<a:.+:(\d+)>/gm
+		input = input.replace(/<a?:.+:\d+>/gm, '')
 
-		const emoj = message.content.match(hasEmoteRegex)
+		await message.channel.sendTyping()
 
-		input = input
-			.replace(emoteRegex.exec(emoj), '')
-			.replace(animatedEmoteRegex.exec(emoj), '')
+		const url = new URL('https://api.affiliateplus.xyz/api/chatbot'),
+			params = url.searchParams,
+			age = new Date().getFullYear() - client.user.createdAt.getFullYear()
 
-		message.channel.sendTyping()
+		params.set('message', input)
+		params.set('ownername', options.developer ?? 'Rahuletto#0243')
+		params.set('botname', options.name ?? client.user.username)
+		params.set('age', age)
+		params.set('birthyear', client.user.createdAt.getFullYear())
+		params.set('birthdate', client.user.createdAt.toLocaleDateString())
+		params.set('birthplace', 'Simply-DJS')
+		params.set('location', message.guild.name)
+		params.set('company', 'Simply-DJS')
+		params.set('user', message.author.id)
 
 		// Using await instead of .then
-		const jsonRes = await fetch(
-			`https://api.affiliateplus.xyz/api/chatbot?message=${input}&botname=${botName}&ownername=${developer}&user=${message.author.id}&birthplace=Simply-DJS`
-		).then((res) => res.json()) // Parsing the JSON
+		const jsonRes = await fetch(url).then((res) => res.json()) // Parsing the JSON
 
 		const chatbotReply = jsonRes.message
 			.replace(/@everyone/g, '`@everyone`') //RegExp with g Flag will replace every @everyone instead of just the first
@@ -77,9 +83,11 @@ async function chatbot(client, message, options = {}) {
 		})
 	} catch (err) {
 		if (err instanceof fetch.FetchError) {
-			message.reply({ content: '**Error:**\n```' + err + '```' }) //Catch errors that happen while fetching
-		}
-		console.log(`Error Occured. | chatbot | Error: ${err.stack}`)
+			if (err.type === 'invalid-json') {
+				message.reply({ content: '**Error:**\n```API offline```' }) //Catch errors that happen while fetching
+				console.log(`Error Occured. | chatbot | Error: API offline`)
+			}
+		} else console.log(`Error Occured. | chatbot | Error: ${err.stack}`)
 	}
 }
 
