484c0b45b220b43a1a3f82c2afeaef25864aceb2
Christian Llupo <jebediah47@protonmail.com>
2022-06-12 17:45:43 +0300

    Added JSdoc to ExtendedInteraction.ts

--- src/interfaces/ExtendedInteraction.ts
+++ src/interfaces/ExtendedInteraction.ts
@@ -1,5 +1,15 @@
 import { CommandInteraction, GuildMember, UserMention } from "discord.js";
 
+/**
+ * A class extension of the CommandInteraction object of the disocrd.js framework
+ * which aims to reduce errors and apply the best developer practices.
+ * @param {member} Discord.GuildMember Member type for CommandInteraction due to it missing.
+ * @param {author} Discord.GuildMember Member type for CommandInteraction due to it missing.
+ * @param {mentions} Discord.UserMention Mention type for CommandInteraction due to it missing.
+ * @param {customId} string Option used by the simply-djs's functions for custom-id checking.
+ * @returns {ExtendedInteraction}}
+ */
+
 export interface ExtendedInteraction extends CommandInteraction {
 	mentions: UserMention;
 	member: GuildMember;
